b15862181b58332e028ca80b02feee9490f78184
elebumm <lmenelaws112@gmail.com>
2017-10-26 16:51:47 -0400

    updated website

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+TweenMax.from("#blue-square", 2, {x:600, ease:Power4.easeOut});
+
+TweenMax.from("#yellow-square", 2, {x:-600, ease:Power4.easeOut});
+
+TweenMax.from("#red-square", 2, {y:600, ease:Power4.easeOut});
